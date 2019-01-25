d6fb7aa94c7ab77dc91496e32bd2a4538f4c6e7b
PlinXXX <etudiant47@sayna.io>
2019-01-25 12:09:21 +0100

    Need some modification

---
+++ app.rb
@@ -0,0 +1,8 @@
+require 'csv'
+require 'bundler'
+Bundler.require
+
+$:.unshift File.expand_path("./../lib", __FILE__)
+require 'application'
+
+Application.new.perform
--- morpion.rb
+++
@@ -1,4 +0,0 @@
-require_relative 'application'
-
-Application.new.perform
-
